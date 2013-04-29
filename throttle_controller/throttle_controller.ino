af331f8f36f813b24325ca9528b43a44f116cf21
James Taylor <james.taylor250@gmail.com>
2013-04-30 08:53:39 +1200

    Initial commit

---
+++ throttle_controller/throttle_controller.ino
@@ -0,0 +1,47 @@
+//Test comment put in by Matt to test he can edit in github
+int throttle_input = 0; //input pin from the throttle lever
+int throttle_output = 0; //output pin to the scooter controller
+int incoming_byte = 0; //stores the incoming serial data
+int serial_active = 0; //determines whether we stay in the serial loop
+int throttle_output_val = 0; // holds mapped output value
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(throttle_input, INPUT);
+  pinMode(throttle_output, OUTPUT);
+  
+Serial.begin(115200);
+}
+
+void serial_control(){
+  while(incoming_byte != 500){
+    if (Serial.available() > 0) {
+      incoming_byte = Serial.read();
+      if(incoming_byte >=0){
+        if(incoming_byte <= 0){
+          analogWrite(throttle_output, incoming_byte);
+        }
+      }
+    }
+  }
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+  if (Serial.available() > 0) {
+       incoming_byte = Serial.read();
+       if(incoming_byte == 500){
+          incoming_byte = 0;
+          serial_control();
+       }
+  }
+  
+  throttle_output_val = map(analogRead(throttle_input), 0, 0123, 0, 255);
+  analogWrite(throttle_output, throttle_output_val);
+  
+  delay(2);
+  
+}
