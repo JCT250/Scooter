021ce5bb15ce4e6f15b09d6395fe62929c7495a9
JCT250 <james.taylor250@gmail.com>
2014-06-25 08:40:24 +1200

    throttle updates

--- Arduino Code/Throttle_Code/Throttle_Code.ino
+++ Arduino Code/Throttle_Code/Throttle_Code.ino
@@ -11,6 +11,7 @@ byte throttle_output_val = 0; // holds mapped output value
 byte start_byte = 0x1B;
 byte stop_byte = 0x0A;
 int old_throttle_output_val = 0; //holds old throttle output val
+int resolution = 2; // this is the value that the output value needs to change by for it to be written
 
 
 const int numReadings = 10;
@@ -53,13 +54,13 @@ void serial_control(){
       throttle_write(inData_mapped);
       Serial.println(inData);
       inData_old = inData;
-   }
-	if(serial_throttle.available()) inData = serial_throttle.read();
+    }
+    if(serial_throttle.available()) inData = serial_throttle.read();
   }
   Serial.println("Going Manual");
-      inData_mapped = map(inData, 0, 255, 54, 201);
-      throttle_write(inData_mapped);
-      Serial.println(inData);
+  inData_mapped = map(inData, 0, 255, 54, 201);
+  throttle_write(inData_mapped);
+  Serial.println(inData);
 }
 
 
@@ -69,26 +70,43 @@ void loop() {
   if (serial_throttle.available()) {
     serial_control();
   }
+ average = analogRead(throttle_input);
+ // total = total - readings[index]; //subtract the old reading at this position in the array from the total         
+ // Serial.print("Ind: ");
+ // Serial.println(index);
+ // readings[index] = analogRead(throttle_input); // read from the sensor:
+ // Serial.print("A-Read: ");
+ // Serial.println(readings[index]);
+ // total = total + readings[index]; // add the reading to the total:
+ // Serial.print("Tot: ");
+ // Serial.println(total);
+ // index ++; // advance to the next position in the array:                      
+ // if (index >= numReadings) // if we're at the end of the array... 
+ // {  
+ //   index = 0; // ...wrap around to the beginning:                           
+ // }
+ // average = total / numReadings; // calculate the average:
+ // Serial.print("Av: ");
+ // Serial.println(average);
 
-  total = total - readings[index]; //subtract the old reading at this position in the array from the total         
-  readings[index] = analogRead(throttle_input); // read from the sensor:  
-  total = total + readings[index]; // add the reading to the total:   
-  index = index + 1; // advance to the next position in the array:                      
-  if (index >= numReadings) // if we're at the end of the array...              
-    index = 0; // ...wrap around to the beginning:                           
-  average = total / numReadings; // calculate the average:
 
   throttle_output_val = map(average, 0, 1023, 255, 0); //map the average to the correct range for output
-  serial_throttle.write((map(throttle_output_val,63,187,100,-100)));
-  if(throttle_output_val != old_throttle_output_val){ //check to see if the value has changed
+  Serial.print("Th-map: ");
+  Serial.println(throttle_output_val);
+  serial_throttle.write((map(throttle_output_val,63,187,100,-100))); // write the reading to the comms nano
+  if(throttle_output_val > old_throttle_output_val+resolution || throttle_output_val < old_throttle_output_val-resolution){ //check to see if the value has changed
+    Serial.print("Th-TX:                 ");
     Serial.println(throttle_output_val); //print the value to the serial port
     throttle_write(throttle_output_val); //analog write the value to the output pin
     old_throttle_output_val = throttle_output_val; //update the current value
   }
- 
+
   delay(2);
 
 }
+
+
+
 
 
 
